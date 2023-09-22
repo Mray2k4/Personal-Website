5df41e297b17229bae740cfe540e29b28444bbf2
Marque Ray <mray2k4@yahoo.com>
2023-09-21 17:57:11 -0700

    added a headshot on About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 export default function about() {
   return (
     <Layout>
+  <img className='' src="/headshot 3.jpg" alt="Headshot 1" />
     <div>
         <h1>About page</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores natus ipsum quis soluta, adipisci mollitia ut accusantium, voluptatem recusandae dolores repellat at minima itaque dolor amet error temporibus dignissimos consequatur.</p>
