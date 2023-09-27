f353ce67ffc8c37215f98c521b3f0b5c9de3a64a
Marque Ray <mray2k4@yahoo.com>
2023-09-26 22:03:22 -0700

    changed About paragraph up a bit

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,9 +9,9 @@ export default function about() {
         <h1>About page</h1>
         <p>Hi my name is Marque Ray and I'm on my way to become a Software Developer.
            I was always interested in computers when I was a teen. I didn't know much 
-           about coding back then but I liked to learn about how to 
-           add software and changed how things looked and worked in the computer. I have been
-           an athlete all my life, which had been my main focus, but I decided to switch focus
+           about coding back then but I liked to learn about software and how to changed 
+          the way things looked and worked in the computer. I have been
+           an athlete all my life, which had been my main focus, so I decided to have a new focus
            and start my journey as a Software Developer.   
         </p>
     </div>
